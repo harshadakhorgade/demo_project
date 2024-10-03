42e0af65c60be534970aae0f1feb687f0ab413c3
harshadakhorgade <harshadakhorgade9@gmail.com>
2024-10-03 17:47:39 +0530

    path updated

--- UserData.sh
+++ UserData.sh
@@ -8,7 +8,7 @@ GIT_REPO_URL="https://github.com/harshadakhorgade/demo_project.git"
 #GIT_REPO_URL="https://<your_username>:<your_PAT>@github.com/codewithmuh/django-aws-ec2-autoscaling.git"
 
 # Replace {YOUR_PROJECT_MAIN_DIR_NAME} with your actual project directory name
-PROJECT_MAIN_DIR_NAME="demo_project"
+PROJECT_MAIN_DIR_NAME="deploy_project/demo_project"
 
 # Clone repository
 git clone "$GIT_REPO_URL" "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"
--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-PROJECT_MAIN_DIR_NAME="demo_project"
+PROJECT_MAIN_DIR_NAME="deploy_project/demo_project"
 
 # Validate variables
 if [ -z "$PROJECT_MAIN_DIR_NAME" ]; then
